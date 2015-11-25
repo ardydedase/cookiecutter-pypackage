c13fc1305500556f28fd1e17ccf9496605a74e8e
ardydedase <ardy.dedase@gmail.com>
2015-11-25 18:31:14 +0800

    Fix class

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-class {{(cookiecutter.project_slug.title()).replace('_', '')}}
+class {{(cookiecutter.project_slug.title()).replace('_', '')}}(object):
 
 	def __init__(self):
 		pass
