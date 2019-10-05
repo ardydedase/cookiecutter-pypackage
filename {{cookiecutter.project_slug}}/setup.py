de8b3ab9cc5d1e95d1bfddef34d8d233a6d77851
Ardy Dedase <ardy.dedase@gmail.com>
2019-10-05 19:18:25 +0800

    Fix for change in pip 10.
    https://stackoverflow.com/questions/25192794/no-module-named-pip-req

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -3,7 +3,10 @@
 
 import pip
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 try:
     from setuptools import setup
